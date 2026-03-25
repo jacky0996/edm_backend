9b0746ee9b397a9b0eee15af8f5f1288ac3790cb
shanelin1 <shane.lin@hwacom.com>
2026-03-25 16:35:10 +0800

    修改容器使用port

--- database/migrations/2026_03_24_080803_create_member_table.php
+++ database/migrations/2026_03_24_080803_create_member_table.php
@@ -14,12 +14,11 @@ return new class extends Migration
         Schema::create('member', function (Blueprint $table) {
             $table->bigInteger('id')->autoIncrement()->unsigned();
             $table->string('name', 10)->comment('姓名');
-            $table->string('national_id', 20)->nullable()->comment('身分證');
             $table->integer('status')->default(0)->comment('狀態，0:尚未驗證, 1:正常');
+            $table->string('sales', 20)->comment('業務');
             $table->timestamps();
             $table->softDeletes();
             $table->index(['name']);
-            $table->index(['national_id']);
         });
     }
 
